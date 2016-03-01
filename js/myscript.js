5da216032a6e9b08c3e072a6a26260fd7e2b51cf
kaji <kanaiwork@gmail.com>
2016-03-02 02:03:21 +0900

    no message

---
+++ js/myscript.js
@@ -0,0 +1,26 @@
+// controller
+angular.module('myApp',[])
+/*.controller('mainCtrl', ['$scope', function($scope){
+  $scope.users = [
+    {"name":"taguchi", "score":52.22},
+    {"name":"tanaka", "score":38.22},
+    {"name":"yamada", "score":11.11},
+    {"name":"satou", "score":12.51},
+    {"name":"suzuki", "score":55.82},
+    {"name":"kaji", "score":10.99},
+    {"name":"nakata", "score":78.44},
+    {"name":"hazama", "score":22.89},
+  ];
+  $scope.today = new Date();
+}])
+
+.controller('userItemCtrl',['$scope', function($scope){
+  $scope.increment = function(){
+    $scope.user.score++;
+  }
+}])*/
+
+.controller('mainCtrl',['$scope', function($scope){
+
+}])
+;
