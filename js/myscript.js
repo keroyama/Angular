606271bbd602a87a5b42c91d55b9459171f4f72e
kaji <kanaiwork@gmail.com>
2016-04-13 01:19:08 +0900

    lesson2

--- js/myscript.js
+++ js/myscript.js
@@ -1,26 +1,11 @@
-// controller
-angular.module('myApp',[])
-/*.controller('mainCtrl', ['$scope', function($scope){
-  $scope.users = [
-    {"name":"taguchi", "score":52.22},
-    {"name":"tanaka", "score":38.22},
-    {"name":"yamada", "score":11.11},
-    {"name":"satou", "score":12.51},
-    {"name":"suzuki", "score":55.82},
-    {"name":"kaji", "score":10.99},
-    {"name":"nakata", "score":78.44},
-    {"name":"hazama", "score":22.89},
-  ];
-  $scope.today = new Date();
+angular.module('myApp', [])
+.controller('mainCtrl',['$scope', 'UserList', function($scope, UserList){
+  $scope.users = UserList();  
 }])
-
-.controller('userItemCtrl',['$scope', function($scope){
-  $scope.increment = function(){
-    $scope.user.score++;
-  }
-}])*/
-
-.controller('mainCtrl',['$scope', function($scope){
-  //$scope.addUser
-}])
-;
+.value('UserList', function(){
+  return [
+    {"name":"taguchi","score":58.22},
+    {"name":"yamada","score":18.45},
+    {"name":"kanai","score":24.77}
+  ]
+});
