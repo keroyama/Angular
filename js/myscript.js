42beecae84c6c016363210820de99a392bb886b9
kaji <kanaiwork@gmail.com>
2016-04-30 00:32:58 +0900

    test

--- js/myscript.js
+++ js/myscript.js
@@ -6,7 +6,7 @@ angular.module('myApp', [])
     $scope.greeting = 'こんんちわ' + $scope.myName + 'さん';
   };*/
 
-  $scope.show = true;
+  /*$scope.show = true;
 
   $scope.onclick = function(){
     angular.element(document.getElementById('panel')).css({
@@ -14,4 +14,29 @@ angular.module('myApp', [])
       color: '#fff'
     });
   };
+
+  $scope.seasons = [
+    {name:'春', value:'spring'},
+    {name:'夏', value:'summer'},
+    {name:'秋', value:'fall'},
+    {name:'冬', value:'winter'}
+  ];
+  $scope.season = $scope.seasons[0].value;
+
+  var prevDay = 1001;
+  var nextDay = 1000;
+  //console.log(number);
+  if(prevDay < nextDay){
+    $scope.obj = '小さい';
+  } else if(prevDay > nextDay) {
+    $scope.obj = '大きい';
+  } else {
+    $scope.obj = '同じ';
+  }*/
+
+  var max = 140;
+  $scope.onchange = function(){
+
+    $scope.count = $scope.tweet.length;
+  }
 }]);
